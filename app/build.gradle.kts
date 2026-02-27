c9af8aa7b6a8b88be9db65d2efb2016da6065b8c
soraiyu <koji.iimura@gmail.com>
2026-02-27 16:27:19 +0000

    Fix release permissions and bump to v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.rtneg.kyuubimask"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
     }
 
     buildTypes {
